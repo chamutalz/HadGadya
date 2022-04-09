787a4afe4e66bf7fa373776c6dcfa83c99dba922
chamutalz <chamutalz@gmail.com>
2022-04-09 21:00:45 +0300

    First commit, mminimal version

---
+++ HadGadya/HadGadya/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace HadGadya
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string hadGadya = File.ReadAllText("C:/Chamutal/Projects/HadGadya/hadGadya.txt");
+			int go = HadGadya(9, hadGadya);
+		}
+
+		public static int HadGadya(int pathIndex, string text)
+		{
+			if (pathIndex <= 0)
+			{
+				
+				return 0;
+			}
+			string path = "C:/Chamutal/Projects/HadGadya/H" + pathIndex.ToString() + ".txt";
+
+			text = File.ReadAllText(path) + "\n" + text;
+			Console.WriteLine(text);
+			return HadGadya(pathIndex - 1, text);
+		}
+	}
+}
